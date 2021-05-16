74edc4e1820b3910d83a92abebc89522a48252c8
Mark Malström <mark@malstrom.me>
2021-05-16 03:44:27 -0500

    Added search

--- PodcastExample/PodcastExample/PodcastList.swift
+++ PodcastExample/PodcastExample/PodcastList.swift
@@ -4,7 +4,23 @@ import Cider
 
 final class PodcastList: UICollectionViewController {
     typealias Section = UICollectionViewController.SingleSection
-    private var cancellables = Set<AnyCancellable>()
+    final class SearchCoordinator: NSObject, UISearchResultsUpdating {
+        @Published var text: String = ""
+        private var updatedText: String?
+        
+        func updateSearchResults(for searchController: UISearchController) {
+            guard let text = searchController.searchBar.text else { return }
+            guard updatedText != text else { return }
+            
+            DispatchQueue.main.async {
+                self.updatedText = text
+                self.text = text
+            }
+        }
+    }
+    
+    private let searchController = UISearchController()
+    private let searchCoordinator = SearchCoordinator()
     
     private lazy var dataSource = UICollectionViewDiffableDataSource<Section, Podcast>(
         collectionView: collectionView,
@@ -19,12 +35,21 @@ final class PodcastList: UICollectionViewController {
         super.viewDidLoad()
         
         title = "Podcasts"
+        
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = searchCoordinator
+        
+        navigationItem.searchController = searchController
         collectionView.dataSource = dataSource
         
-        PodcastManager()
-            .search(for: "Headgum")
-            .assertNoFailure()
+        searchCoordinator.$text
+            .debounce(for: 0.3, scheduler: DispatchQueue.main)
+            .flatMap { PodcastManager().search(for: $0) }
+            .replaceError(with: [])
+            .receive(on: DispatchQueue.main)
             .sink(receiveValue: update(with:))
             .store(in: &cancellables)
     }
+    
+    private var cancellables = Set<AnyCancellable>()
 }
