05039b2895b009e0e34a539569b3e01b3dd61132
Mark Malström <mark@malstrom.me>
2021-05-18 00:44:16 -0500

    Added SwiftUI-like APIs to the layout DSLs, including `frame` and `padding`

--- Sources/Cider/Layout.swift
+++ Sources/Cider/Layout.swift
@@ -37,6 +37,14 @@ public func ==(lhs: NSLayoutDimension, rhs: CGFloat) -> NSLayoutConstraint {
     lhs.constraint(equalToConstant: rhs)
 }
 
+public func <=(lhs: NSLayoutDimension, rhs: CGFloat) -> NSLayoutConstraint {
+    lhs.constraint(lessThanOrEqualToConstant: rhs)
+}
+
+public func >=(lhs: NSLayoutDimension, rhs: CGFloat) -> NSLayoutConstraint {
+    lhs.constraint(greaterThanOrEqualToConstant: rhs)
+}
+
 public func -<Anchor: AnyObject>(lhs: NSLayoutAnchor<Anchor>, rhs: CGFloat) -> (NSLayoutAnchor<Anchor>, CGFloat) {
     return (lhs, -rhs)
 }
@@ -69,6 +77,81 @@ extension UIView {
     public func layout(@NSLayoutConstraint.ConstraintBuilder using closure: (UIView) -> [NSLayoutConstraint]) {
         translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(closure(self))
+    }
+    
+    public func frame(width: CGFloat? = nil, height: CGFloat? = nil) -> [NSLayoutConstraint] {
+        var constraints: [NSLayoutConstraint] = []
+        
+        if let width = width {
+            constraints.append(widthAnchor == width)
+        }
+        
+        if let height = height {
+            constraints.append(heightAnchor == height)
+        }
+        
+        return constraints
+    }
+    
+    public func frame(size: CGFloat? = nil) -> [NSLayoutConstraint] {
+        var constraints: [NSLayoutConstraint] = []
+        
+        if let size = size {
+            constraints = [
+                widthAnchor == size,
+                heightAnchor == size
+            ]
+        }
+        
+        return constraints
+    }
+    
+    public func frame(
+        minWidth: CGFloat? = nil,
+        maxWidth: CGFloat? = nil,
+        minHeight: CGFloat? = nil,
+        maxHeight: CGFloat? = nil
+    ) -> [NSLayoutConstraint] {
+        var constraints: [NSLayoutConstraint] = []
+        
+        if let minWidth = minWidth {
+            constraints.append(widthAnchor >= minWidth)
+        }
+        
+        if let maxWidth = maxWidth {
+            constraints.append(widthAnchor <= maxWidth)
+        }
+        
+        if let minHeight = minHeight {
+            constraints.append(widthAnchor >= minHeight)
+        }
+        
+        if let maxHeight = maxHeight {
+            constraints.append(widthAnchor <= maxHeight)
+        }
+        
+        return constraints
+    }
+    
+    public func padding(_ insets: UIEdgeInsets) {
+        self.layoutMargins = insets
+    }
+    
+    public func padding(_ length: CGFloat) {
+        self.padding(.init(top: length, left: length, bottom: length, right: length))
+    }
+    
+    func padding(_ edges: NSDirectionalRectEdge = .all, _ length: CGFloat? = nil) {
+        if let length = length {
+            switch edges {
+            case .all: self.padding(length)
+            case .top: self.layoutMargins.top = length
+            case .bottom: self.layoutMargins.bottom = length
+            case .leading: self.layoutMargins.left = length
+            case .trailing: self.layoutMargins.right = length
+            default: break
+            }
+        }
     }
     
     public func addSubviews(_ views: [UIView]) {
--- Sources/Cider/SnapKit.swift
+++ Sources/Cider/SnapKit.swift
@@ -1,6 +1,68 @@
 import UIKit
 import SnapKit
 
+extension ConstraintMaker {
+    public func frame(width: CGFloat? = nil, height: CGFloat? = nil) {
+        if let width = width {
+            self.width == width
+        }
+        
+        if let height = height {
+            self.height == height
+        }
+    }
+    
+    public func frame(size: CGFloat? = nil) {
+        if let size = size {
+            self.size == size
+        }
+    }
+    
+    public func frame(
+        minWidth: CGFloat? = nil,
+        maxWidth: CGFloat? = nil,
+        minHeight: CGFloat? = nil,
+        maxHeight: CGFloat? = nil
+    ) {
+        if let minWidth = minWidth {
+            self.width >= minWidth
+        }
+        
+        if let maxWidth = maxWidth {
+            self.width <= maxWidth
+        }
+        
+        if let minHeight = minHeight {
+            self.width >= minHeight
+        }
+        
+        if let maxHeight = maxHeight {
+            self.width <= maxHeight
+        }
+    }
+    
+    public func padding(_ insets: UIEdgeInsets) {
+        self.margins == insets
+    }
+    
+    public func padding(_ length: CGFloat) {
+        self.margins == length
+    }
+    
+    func padding(_ edges: NSDirectionalRectEdge = .all, _ length: CGFloat? = nil) {
+        if let length = length {
+            switch edges {
+            case .all: self.margins == length
+            case .top: self.topMargin == length
+            case .bottom: self.bottomMargin == length
+            case .leading: self.leadingMargin == length
+            case .trailing: self.trailingMargin == length
+            default: break
+            }
+        }
+    }
+}
+
 public func ==(lhs: ConstraintMakerExtendable, rhs: ConstraintRelatableTarget) {
     lhs.equalTo(rhs)
 }
